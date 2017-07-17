4ab3b4b1ea3a227de10961be311fbc4040716ab4
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-07-16 21:04:47 -0700

    Pictures get populated to the screen

--- app.js
+++ app.js
@@ -1,52 +1,71 @@
-var topics = ["Javascript", "Technology", "Coding", "SpaceX", "Metallica", "Icecream"];
+  var topics = ["Japan", "Technology", "Coding", "SpaceX", "Metallica", "Icecream"];
 
-appendButtons(topics);
-
-
-$('#buttons .button').on('click', function(){
-  var topic = $(this).text();
-  console.log(topic);
-  var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+ topic+"&api_key=c7fd486fe62d4cfdb33110a0b2750064&limit=5");
-  xhr.done(function(data) {
-
-    console.log("success got data", data);
-    console.log(data.data[0].images.original.url);
-    console.log(data.data[0].images.original_still.url);
-
-  });
+  appendButtons(topics);
 
 
-})
+  $('#buttons .button').on('click', ajaxCall);
 
 
 
 // Response returns an object back with an object with a property that is an arrray
 
-$('#submit').on('click', function(){
+  $('#submit').on('click', function(){
 
-
-})
-function createButtons(array){
-  for(var i = 0; i < array.length; i++){
-
-    var gif = data.data[i].images.original.url;
-    var still = data.data[i].images.orinal_still.url;
-
-    var el = $('img');
-    el.attr('src', still);
-    el.attr('data-gif', gif);
-    }
-
-}
-
-// creates buttons from an array with strings
-
-function appendButtons (array){
-  for(var i = 0; i < array.length; i++){
+    var input = $('#search').val();
+    console.log($(this));
     var el = $('<div>');
       el.addClass('btn btn-primary btn-lg button');
-      el.text(array[i]);
+      el.text(input);
 
     $('#buttons').append(el);
+
+    $('#buttons .button').on('click', ajaxCall);
+
+  });
+
+  function createButtons(array){
+    for(var i = 0; i < array.length; i++){
+
+      var gif = data.data[i].images.original.url;
+      var still = data.data[i].images.orinal_still.url;
+
+      var el = $('img');
+      el.attr('src', still);
+      el.attr('data-gif', gif);
+      }
+
   }
-}
+
+  // creates buttons from an array with strings
+
+  function appendButtons (array){
+    for(var i = 0; i < array.length; i++){
+      var el = $('<div>');
+        el.addClass('btn btn-primary btn-lg button');
+        el.text(array[i]);
+
+      $('#buttons').append(el);
+    }
+  }
+
+
+  function ajaxCall (){
+    var topic = $(this).text();
+    console.log(topic);
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+ topic+"&api_key=c7fd486fe62d4cfdb33110a0b2750064&limit=5");
+    xhr.done(function(data) {
+
+      $('#gifs').html('');
+      for( var i = 0; i < 5; i++){
+        var gif = data.data[i].images.original.url;
+        var still = data.data[i].images.original_still.url;
+        var img = $('<img>');
+            img.attr('src', still);
+            img.attr('style','width:150px;width:150px;')
+            $('#gifs').append(img);
+        console.log("success got data", data);
+        console.log(data.data[0].images.original.url);
+        console.log(data.data[0].images.original_still.url);
+      }
+    });
+  }
